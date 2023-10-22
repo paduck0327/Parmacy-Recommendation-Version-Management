9acfdf801137d4d27a364429059f64755d518d71
paduck0327 <paduck0327@gmail.com>
2023-10-22 20:11:38 +0900

    first commit

---
+++ src/main/java/com/example/projcet/ProjcetApplication.java
@@ -0,0 +1,13 @@
+package com.example.projcet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjcetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjcetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projcet/ProjcetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projcet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjcetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
